647de134e730b01a3e93a6b461a2984dcff06119
wesley9mm <info.developer4@gmail.com>
2018-06-19 23:27:29 -0700

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//create a trivia  true/false options (your choice).
+
+//The player will have a limited amount of time to finish the quiz. (30 secs)
+        
+//The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+        
+//Don't let the player pick more than one answer per question.
+        
+//include a countdown timer. 
